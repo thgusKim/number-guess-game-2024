cee048f92ded1aeb6791000ff52bd94861aa909c
Sohyeon Kim <thgus7772@naver.com>
2024-02-12 22:16:48 +0900

    second commit

--- main.js
+++ main.js
@@ -12,23 +12,66 @@ let computerNum = 0;
 let playButton = document.getElementById("play-button");
 let userInput = document.getElementById("user-input");
 let resultArea = document.getElementById("result-area");
+let resetButton = document.getElementById("reset-button");
+let chances = 5;
+let gameOver = false;
+let chanceArea = document.getElementById("chance-area");
+let history = [];
 
-playButton.addEventListener("click", play)
+playButton.addEventListener("click", play);
+resetButton.addEventListener("click", reset);;
+userInput.addEventListener("focus", function () {
+    userInput.value = "";
+})
 
 function pickRandomNum() {
-  computerNum = Math.floor(Math.random() * 100) + 1;
-  console.log("정답", computerNum);
+    computerNum = Math.floor(Math.random() * 100) + 1;
+    console.log("정답", computerNum);
 }
 
-function play(){
+function play() {
     let userValue = userInput.value;
-    if(userValue < computerNum){
+
+    if (userValue < 1 || userValue > 100) {
+        resultArea.textContent = "1과 100 사이 숫자를 입력해주세요.";
+        return;
+    }
+
+    if (history.includes(userValue)) {
+        resultArea.textContent = "이미 입력한 숫자입니다. 다른 숫자를 입력해주세요.";
+        return;
+    }
+
+    chances--;
+    chanceArea.textContent = `남은 기회:${chances}번`;
+
+    if (userValue < computerNum) {
         resultArea.textContent = "Up!!!";
-    }else if(userValue > computerNum){
+    } else if (userValue > computerNum) {
         resultArea.textContent = "Down!!!";
-    }else{
+    } else {
         resultArea.textContent = "맞췄습니다!!!";
+        gameOver = true;
+    }
+
+    history.push(userValue);
+    console.log(history);
+
+    if (chances < 1) {
+        gameOver = true;
+    }
+
+    if (gameOver == true) {
+        playButton.disabled = true;
     }
 }
 
+function reset() {
+    //user input 창이 깨끗하게 정리되고
+    userInput.value = "";
+    //새로운 번호가 생성
+    pickRandomNum();
+
+    resultArea.textContent = "결과 값이 여기 나옵니다!";
+}
 pickRandomNum();
